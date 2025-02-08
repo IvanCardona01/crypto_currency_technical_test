6bb79f683bdca76d4e0130e14ecc0109f4860af6
Ivan Cardona <2021ivancardona@gmail.com>
2025-02-08 18:22:56 -0500

    - Added necessary provider for network cache and safe area - Added app icon, and splash screen - Created React Query Client

---
+++ src/screens/crypto-list/crypto-list-screen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CryptoListScreen() {
+  return (
+    <View>
+      <Text>CryptoListScreen</Text>
+    </View>
+  )
+}
